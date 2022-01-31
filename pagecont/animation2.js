411459bf85573bdd2833f9387f127eb02aa2ca53
drainkid <elmirrzaevv@gmail.com>
2022-01-31 16:43:52 +1000

    исправил позор

--- pagecont/animation2.js
+++ pagecont/animation2.js
@@ -1,22 +1,22 @@
 $(document).ready(function (){
 
-    $('.v3_32').fadeOut('fast', 'swing');
-    $('.v10_2').fadeOut('fast', 'swing');
-    $('.v10_4').fadeOut('fast', 'swing');
-    $('.v10_3').fadeOut('fast', 'swing');
+    $('.fedorfet').fadeOut('fast', 'swing');
+    $('.drainkid').fadeOut('fast', 'swing');
+    $('.allcash').fadeOut('fast', 'swing');
+    $('.ruiner').fadeOut('fast', 'swing');
 
     $(window).scroll(function () {
 
 
         if ($(this).scrollTop() > 200) {
 
-            $('.v3_32').fadeIn('slow', 'swing');
-            $('.v10_2').fadeIn('slow', 'swing');
+            $('.fedorfet').fadeIn('slow', 'swing');
+            $('.drainkid').fadeIn('slow', 'swing');
 
         }
         if ($(this).scrollTop() > 1000) {
-            $('.v10_4').fadeIn('slow', 'swing');
-            $('.v10_3').fadeIn('slow', 'swing');
+            $('.allcash').fadeIn('slow', 'swing');
+            $('.ruiner').fadeIn('slow', 'swing');
         }
 
     });
